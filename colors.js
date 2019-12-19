72c1aadefd740fa589c68b1095c499e3a17b1dc9
im5439 <57483743+im5439@users.noreply.github.com>
2019-12-19 16:51:01 +0900

    Use Jquery

--- colors.js
+++ colors.js
@@ -1,21 +1,25 @@
 var Links = {
-  setColor:function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while (i < alist.length) {
-      alist[i].style.color = color;
-      i = i + 1;
+  setColor: function(color) {
+    //   var alist = document.querySelectorAll('a');
+    //   var i = 0;
+    //   while (i < alist.length) {
+    //     alist[i].style.color = color;
+    //     i = i + 1;
+    // }
+    $('a').css('color', color); // jquery / CDN(Content Delivery Network)를 지원
   }
-}
 }
 var Body = {
-  setColor:function(color) {
-    document.querySelector('body').style.color = color;
+  setColor: function(color) {
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color); // jquery
   }, // 객체는 객체의 property와 property를 구분할떄 ','를 찍는다
-  setBackgroundColor:function(color) {
-    document.querySelector('body').style.backgroundColor = color;
+  setBackgroundColor: function(color) {
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color); // jquery
   }
 }
+
 function nightDayHandler(self) {
   if (self.value === 'night') {
     Body.setBackgroundColor('black');
